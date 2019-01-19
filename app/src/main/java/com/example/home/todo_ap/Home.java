0fb2b5efc7ed6130c3c87a807e055f1d1d5916b4
aryankh77 <aryankh932@gmail.com>
2019-01-19 16:05:28 +0330

    commit tamom karadane rize karye register

--- app/src/main/java/com/example/home/todo_ap/Home.java
+++ app/src/main/java/com/example/home/todo_ap/Home.java
@@ -3,22 +3,32 @@ package com.example.home.todo_ap;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Home extends AppCompatActivity {
     FloatingActionButton fab;
     RecyclerView recyclerView;
     NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         recyclerView = findViewById(R.id.recyclerViewTasks);
         fab = findViewById(R.id.fab);
+        drawerLayout=findViewById(R.id.dl);
+        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         navigationView=findViewById(R.id.ngv);
         navigationView.setItemIconTintList(null);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -31,4 +41,12 @@ public class Home extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
--- app/src/main/java/com/example/home/todo_ap/Register.java
+++ app/src/main/java/com/example/home/todo_ap/Register.java
@@ -20,13 +20,14 @@ public class Register extends AppCompatActivity {
     private Button confirm;
     private RadioGroup radioGroup;
     private RadioButton radioButton;
-    boolean u=false;
-    boolean n=false;
-    boolean e=false;
-    boolean p=false;
+    boolean u = false;
+    boolean n = false;
+    boolean e = false;
+    boolean p = false;
     String select;
     static ObjectInputStream objectInputStream;
     static ObjectOutputStream objectOutputStream;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,18 +55,23 @@ public class Register extends AppCompatActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
+
             // the user's changes are saved here
             public void onTextChanged(CharSequence c, int start, int before, int count) {
             }
+
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length()==0||chekint(s)) {
-                    userName.setError("you must fill your username or you start with number");
-                    u=false;
+                if (s.length() == 0 || chekint(s) || chekword(s)) {
+                    userName.setError("you must fill your username or you start with number or put bad word ");
+                    u = false;
                     invisibel(confirm);
-                }else {
+                } else {
                     //visibel(confirm);
-                    u=true;
+                    u = true;
+                }
+                if (u == true && p == true && n == true && e == true) {
+                    visibel(confirm);
                 }
 
 
@@ -86,25 +92,25 @@ public class Register extends AppCompatActivity {
                 if (c.length() < 8) {
                     password.setError("password must be more than 8 chracter");
                     invisibel(confirm);
-                    p=false;
-                }else {
-                    p=true;
+                    p = false;
+                } else {
+                    p = true;
                 }
-
 
 
             }
+
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length()==0){
-                    password.setError("you must fill your password");
+                if (s.length() == 0||!checkuln(s)) {
+                    password.setError("you must fill your password or you donot use upper and lower word or you do not use num");
                     invisibel(confirm);
-                    p=false;
-                }else {
+                    p = false;
+                } else {
                     //visibel(confirm);
-                    p=true;
+                    p = true;
                 }
-                if(u==true&&p==true&&n==true&&e==true){
+                if (u == true && p == true && n == true && e == true) {
                     visibel(confirm);
                 }
             }
@@ -119,43 +125,41 @@ public class Register extends AppCompatActivity {
 
             // the user's changes are saved here
             public void onTextChanged(CharSequence c, int start, int before, int count) {
-                boolean is=false;
-                for (int i = 0; i <c.length() ; i++) {
-                    if (c.toString().charAt(i)== '@') {
-                        if(c.toString().endsWith(".com"))
-                        {
+                boolean is = false;
+                for (int i = 0; i < c.length(); i++) {
+                    if (c.toString().charAt(i) == '@') {
+                        if (c.toString().endsWith(".com") || c.toString().endsWith(".ir")) {
                             is = true;
-                            e=true;
+                            e = true;
                             break;
                         }
                     }
                 }
-                if(is==false){
+                if (is == false) {
                     email.setError("email notvalid it must have @ and .com");
                     invisibel(confirm);
-                    e=false;
+                    e = false;
                 }
-
 
 
             }
+
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length()==0||checkword(s)) {
+                if (s.length() == 0 || checkword(s)) {
                     email.setError("you must fill your email or you put notvalid word ");
                     invisibel(confirm);
-                    e=false;
-                }
-                else {
+                    e = false;
+                } else {
                     //visibel(confirm);
-                    e=true;
+                    e = true;
                 }
                 /*if(checkword(s)){
                     email.setError("email start with nonvalid word");
                     invisibel(confirm);
                 }*/
 
-                if(u==true&&p==true&&n==true&&e==true){
+                if (u == true && p == true && n == true && e == true) {
                     visibel(confirm);
                 }
 
@@ -172,17 +176,18 @@ public class Register extends AppCompatActivity {
             // the user's changes are saved here
             public void onTextChanged(CharSequence c, int start, int before, int count) {
             }
+
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length()==0) {
+                if (s.length() == 0) {
                     name.setError("you must fill your name");
                     invisibel(confirm);
-                    n=false;
-                }else {
+                    n = false;
+                } else {
                     //visibel(confirm);
-                    n=true;
+                    n = true;
                 }
-                if(u==true&&p==true&&n==true&&e==true){
+                if (u == true && p == true && n == true && e == true) {
                     visibel(confirm);
                 }
 
@@ -194,12 +199,12 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkbutton(radioButton);
-                select=radioButton.getText().toString();
-                String un=userName.getText().toString();
-                String pa=password.getText().toString();
-                String em=email.getText().toString();
-                String na=name.getText().toString();
-                String fn=familyName.getText().toString();
+                select = radioButton.getText().toString();
+                String un = userName.getText().toString();
+                String pa = password.getText().toString();
+                String em = email.getText().toString();
+                String na = name.getText().toString();
+                String fn = familyName.getText().toString();
                 /*try {
                     objectOutputStream.writeUTF("register");
                     objectOutputStream.writeUTF(un);
@@ -211,7 +216,7 @@ public class Register extends AppCompatActivity {
                 }catch (Exception io){
                 }*/
 
-                Intent intent =new Intent(Register.this,Home.class);
+                Intent intent = new Intent(Register.this, Home.class);
                 startActivity(intent);
                 finish();
 
@@ -220,10 +225,6 @@ public class Register extends AppCompatActivity {
 
 
     }
-
-
-
-
 
 
     public void checkbutton(View v) {
@@ -235,26 +236,83 @@ public class Register extends AppCompatActivity {
 
         confirm.setVisibility(View.VISIBLE);
     }
-    public void invisibel(View v){
+
+    public void invisibel(View v) {
         confirm.setVisibility(View.INVISIBLE);
     }
-    public boolean chekint(Editable c){
-        if(c.charAt(0)=='0'||c.charAt(0)=='1'||c.charAt(0)=='2'||c.charAt(0)=='3'||c.charAt(0)=='4'||c.charAt(0)=='5'||c.charAt(0)=='6'||c.charAt(0)=='7'||c.charAt(0)=='8'||c.charAt(0)=='9'){
+
+    public boolean chekint(Editable c) {
+        if (c.charAt(0) == '0' || c.charAt(0) == '1' || c.charAt(0) == '2' || c.charAt(0) == '3' || c.charAt(0) == '4' || c.charAt(0) == '5' || c.charAt(0) == '6' || c.charAt(0) == '7' || c.charAt(0) == '8' || c.charAt(0) == '9') {
             return true;
-        }else {
+        } else {
             return false;
         }
 
     }
-    public boolean checkword(Editable c){
-        if(c.charAt(0)=='/'||c.charAt(0)=='?'||c.charAt(0)=='.'||c.charAt(0)=='>'||c.charAt(0)==','||c.charAt(0)=='<'||c.charAt(0)=='`'||c.charAt(0)=='~'||c.charAt(0)=='!'||c.charAt(0)=='@'
-                ||c.charAt(0)=='#'||c.charAt(0)=='$'||c.charAt(0)=='%'||c.charAt(0)=='^'||c.charAt(0)=='&'||c.charAt(0)=='*'||c.charAt(0)=='('||c.charAt(0)==')'||c.charAt(0)=='-'||c.charAt(0)=='_'||c.charAt(0)=='='||c.charAt(0)=='+'||c.charAt(0)=='|'||c.charAt(0)=='"'||c.charAt(0)==';'||c.charAt(0)==':'
-                ||c.charAt(0)==']'||c.charAt(0)=='}'||c.charAt(0)=='['||c.charAt(0)=='{'){
-            return true;
-        }else {
-            return false;
+
+    public boolean checkword(Editable c) {
+        boolean f=false;
+        for (int i = 0; i <c.length() ; i++) {
+            if (c.charAt(0) == '/' || c.charAt(0) == '?' || c.charAt(0) == '.' || c.charAt(0) == '>' || c.charAt(0) == ',' || c.charAt(0) == '<' || c.charAt(0) == '`' || c.charAt(0) == '~' || c.charAt(0) == '!' || c.charAt(0) == '@'
+                    || c.charAt(0) == '#' || c.charAt(0) == '$' || c.charAt(0) == '%' || c.charAt(0) == '^' || c.charAt(0) == '&' || c.charAt(0) == '*' || c.charAt(0) == '(' || c.charAt(0) == ')' || c.charAt(0) == '-' || c.charAt(0) == '_' || c.charAt(0) == '=' || c.charAt(0) == '+' || c.charAt(0) == '|' || c.charAt(0) == '"' || c.charAt(0) == ';' || c.charAt(0) == ':'
+                    || c.charAt(0) == ']' || c.charAt(0) == '}' || c.charAt(0) == '[' || c.charAt(0) == '{') {
+                return f;
+            } else {
+                return f;
+            }
         }
+        return f;
+
 
     }
+
+    public boolean chekword(Editable c) {
+        boolean f=false;
+        for (int i = 0; i < c.length(); i++) {
+            if (c.charAt(i) == '/' || c.charAt(i) == '?' || c.charAt(i) == '.' || c.charAt(i) == '>' || c.charAt(i) == ',' || c.charAt(i) == '<' || c.charAt(i) == '`' || c.charAt(i) == '~' || c.charAt(i) == '!' || c.charAt(i) == '@'
+                    || c.charAt(i) == '#' || c.charAt(i) == '$' || c.charAt(i) == '%' || c.charAt(i) == '^' || c.charAt(i) == '&' || c.charAt(i) == '*' || c.charAt(i) == '(' || c.charAt(i) == ')' || c.charAt(i) == '-' || c.charAt(i) == '_' || c.charAt(i) == '=' || c.charAt(i) == '+' || c.charAt(i) == '|' || c.charAt(i) == '"' || c.charAt(i) == ';' || c.charAt(i) == ':'
+                    || c.charAt(i) == ']' || c.charAt(i) == '}' || c.charAt(i) == '[' || c.charAt(i) == '{') {
+                f =true;
+            } else {
+                f= false;
+            }
+
+        }
+        return f;
     }
+
+    public static boolean checkuln(Editable c) {
+        boolean up = false;
+        boolean up1 = false;
+        boolean up2 = false;
+        boolean up3 = false;
+        for (int i = 0; i < c.length(); i++) {
+            if (c.charAt(i) == '0' || c.charAt(i) == '1' || c.charAt(i) == '2' || c.charAt(i) == '3' || c.charAt(i) == '4' || c.charAt(i) == '5' ||
+                    c.charAt(i) == '6' || c.charAt(i) == '7' || c.charAt(i) == '8' || c.charAt(i) == '9') {
+                up = true;
+
+            }
+            else if (Character.isUpperCase(c.charAt(i))) {
+                up1 = true;
+
+            } else if (Character.isLowerCase(c.charAt(i))) {
+                up2 = true;
+
+            } else {
+                up = false;
+                up1 = false;
+                up2 = false;
+            }
+
+        }
+        if (up && up1 && up2) {
+            up3 = true;
+        }
+
+        return up3;
+    }
+    }
+
+
+
 
